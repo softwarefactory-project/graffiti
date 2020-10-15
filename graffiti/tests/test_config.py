b32ee812c3fdc24b8c7df8330bd21c06b6ab43b6
Alfredo Moralejo <amoralej@redhat.com>
2020-10-15 12:58:23 +0200

    Group all tag actions for same tag
    Currently we are processing tag/untag actions for each build instead of
    doing it for each tag. This is suboptimal for CentOS automation as when
    it receives a tag action wait for next event and check if it's for the
    same tag in order to group it. This is broken when we do:
    
     - tag p1 testing -> untag p1 candidate -> tag p2 testing -> untag p2 candidate
    
    This patch changes the sequence to:
    
     - tag p1 testing -> tag p2 testing -> untag p1 candidate -> untag p2 candidate
    
    Also it's grouping all tag or untag actions for the same tag in a single
    multicall.
    
    This patch is also fixing unit tests to work in python3 and python2.
    
    Also i'm moving CI jobs definition to a local .zuul.yaml file to ease
    job definitions and assignment as compared to local configuration in
    config project.
    
    Change-Id: Ia5afab7a98ec003cd3474edf13d877291befb0d2

--- graffiti/tests/test_config.py
+++ graffiti/tests/test_config.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import os.path
+import six
 try:
     import unittest.mock as mock
 except Exception:
@@ -36,7 +37,7 @@ def test_parse_config_file():
     m = mock.mock_open(read_data=SAMPLE_CONFIG)
     # FIXME: the multiple context managers syntax does
     # not work encapsulated by parenthesis hence disable PEP8 checks
-    with mock.patch('__builtin__.open', m), \
+    with mock.patch('six.moves.builtins.open', m), \
          mock.patch('graffiti.config._ensure_rdoinfo',
                     side_effect=_ensure_rdoinfo):  # noqa
         info = parse_config_file('test')
